7441efe2842a0558746016c4233e52210209c42e
RISHANT <rishantdas2003@gmail.com>
2024-01-24 20:00:06 +0530

    Maine yeh bnaya

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
             Hello there! I'm Rishant Das, a passionate and innovative web developer dedicated to crafting exceptional online experiences. With a keen eye for design, a love for clean and efficient code, and an unwavering commitment to staying up-to-date with the latest web technologies, I bring a unique blend of creativity and technical expertise to every project I undertake.
            </p>
            <p>
-           My journey in the world of web development began just a normal 1st year student who was curious about web and all started with youtube tutorials. Since then, I have been on a constant quest to push the boundaries of what's possible in the digital realm. Whether it's building responsive and user-friendly websites, developing interactive web applications, or optimizing for performance and accessibility, I thrive on turning ideas into reality through elegant and functional web solutions.
+            Journey in the world of web development began just a normal 1st year student who was curious about web and all started with youtube tutorials. Since then, I have been on a constant quest to push the boundaries of what's possible in the digital realm. Whether it's building responsive and user-friendly websites, developing interactive web applications, or optimizing for performance and accessibility, I thrive on turning ideas into reality through elegant and functional web solutions.
            </p>
            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
